9982663f1a312aa1dd5ca9baab25f32a8b560d3f
Vishal48github <bhosalevd51@gmail.com>
2025-01-18 23:03:36 +0530

    TL modified the App.java

--- MavenProj01/src/main/java/com/nt/service/App.java
+++ MavenProj01/src/main/java/com/nt/service/App.java
@@ -3,8 +3,17 @@ package com.nt.service;
 /**
  * Hello world!
  */
+
+
 public class App {
+	
+    public int add(int x,int y) {
+    	return x+y;
+    }
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        App app =new App();
+        System.out.println("sum is "+app.add(8, 4));
     }
 }
